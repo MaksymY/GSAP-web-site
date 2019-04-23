65e31b90baed8a6b741cc7f57ab97bde271c767f
MaksymY <maksym.yankivskyypro@gmail.com>
2019-04-23 23:11:52 +0200

    add some animation GSAP

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 window.onload = function() {
+
     let tl1 = new TimelineMax();
 
     tl1
        .from('.sphere', 0.8, {delay:0.1, scale: 0, top:"50%", opacity: 0, ease: Back.easeOut.config(1.7)})
-       .to('intro-container', 1, {top: 80, left: 82, rigth: 82, bottom: 80}, '+0.15')
+
+       .to('.intro-container', 1, {top: 80, left: 82, right: 82, bottom: 80}, '+0.15')
+
+       .to('.sphere', 1, {left: '75%', ease: Back.easeOut.config(2.7)})
+
+       .from('.firstH1', 0.5, {opacity: 0, right:"200%"}, '-=1')
+
+       .from('.secondH1', 0.7, {opacity:0, right:"200%"}, '-=1')
 }
